8a13bc42d0178d0b8cdb8dd01406d45708ef7dc4
gunho <parkgh3737@naver.com>
2019-09-28 04:17:11 +0900

    commit

---
+++ CrackMe(CTF)/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CrackMe_CTF_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string passw0rd = "1234";
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            PWEncryption(textBox2.Text);
+            if ((textBox1.Text == "Adm1n") && textBox2.Text == passw0rd)
+            {
+                MessageBox.Show("하아아아아아아아아위위이이이이이이이?!?!!!", "Success!!!");
+            }
+            else
+            {
+                MessageBox.Show("바위이이이...", "Fail...");
+            }
+            textBox2.Text = "";
+        }
+
+        private void PWEncryption(string pw)
+        {
+            string realPW = "";
+            if (pw.Length == 4)
+            {
+                realPW += textBox2.Text.Substring(3,1);
+                realPW += textBox2.Text.Substring(2, 1);
+                realPW += textBox2.Text.Substring(1, 1);
+                realPW += textBox2.Text.Substring(0, 1);
+                textBox2.Text = realPW;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # *Brute* *Force* *Attack*
 
-### .NET Framework 4.0
+### ENV : .NET Framework 4.0
 
+
+
+### For Test : JackpotProj_VB
+
+
+
+### For REAL : JackpotProj_WindowsFormsApp
+
+
+
+---
+
+
+
+### CrackME(CTF)
+
+##### Team Internal CTF - 2019.09.29
