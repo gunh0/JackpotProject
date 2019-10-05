d231ba8aee35ef42232056b3be0423447180e0f7
gunho <parkgh3737@naver.com>
2019-10-06 04:41:17 +0900

    commit

---
+++ CrackMe2(CTF)/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CrackMe_CTF_
+{
+    public partial class Form1 : Form
+    {
+        string passw0rd = "\"Adm1n\"";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            PWEncryption(textBox2.Text);
+            if ((textBox1.Text == "Adm1n") && textBox2.Text == passw0rd)
+            {
+                MessageBox.Show("하아아아아아아아아위위이이이이이이이?!?!!!", "Success!!!");
+            }
+            else
+            {
+                MessageBox.Show("바위이이이...", "Fail...");
+            }
+            textBox2.Text = "";
+        }
+
+        private void PWEncryption(string pw)
+        {
+            string realPW = passw0rd;
+            textBox2.Text = realPW;
+        }
+    }
+}
---
+++ CrackMe2(CTF)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace CrackMe_CTF_
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
