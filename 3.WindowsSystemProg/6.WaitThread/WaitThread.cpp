a8dc1e1b36593bc7e0a3a32f875586da1bbf7f2e
gunho <parkgh3737@naver.com>
2019-11-01 00:51:31 +0900

    WaitThread completion

--- 3.WindowsSystemProg/6.WaitThread/WaitThread.cpp
+++ 3.WindowsSystemProg/6.WaitThread/WaitThread.cpp
@@ -21,13 +21,16 @@ void _tmain()
 
 	DWORD dwThreadID = 0;
 	HANDLE hThread = CreateThread(NULL, 0, ThreadProc, (PVOID)5000, 0, &dwThreadID);
+	// 스레드 hThread를 생성한다.
 
-	Sleep(10000);
+	//Sleep(10000);
+	WaitForSingleObject(hThread, INFINITE);
+	// 생성한 hThread가 종료되기를 기다린다.
 
 	DWORD dwExitCode;
 	GetExitCodeThread(hThread, &dwExitCode);
-	cout << "Sub thread " << dwThreadID << " terminated. ";
+	cout << "\nSub thread " << dwThreadID << " terminated. " << endl;
 	cout << "Exit code=" << dwExitCode << endl;
 	CloseHandle(hThread);
+	// 스레드가 종료되면 종료 코드를 출력해주고 hThread의 핸들을 닫는다.
 }
-
